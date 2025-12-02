17178056cbd4e5cc3b920db92de1fa335a301183
repied <repied@users.noreply.github.com>
2025-12-02 13:01:21 +0100

    Fix next depth calculation

--- src/gf.ts
+++ src/gf.ts
@@ -285,7 +285,7 @@ export function calculatePlan(diveParams: DiveParams): Plan {
     while (currentDepth >= lastStopDepth) {
         // Find the next stop depth:
         const remaining_to_laststop = currentDepth - lastStopDepth;
-        const n_full_intervals = Math.floor((remaining_to_laststop + 0.00001) / stopInterval);
+        const n_full_intervals = Math.floor((remaining_to_laststop - 0.00001) / stopInterval);
         let nextDepth = lastStopDepth + stopInterval * n_full_intervals;
         if (currentDepth == lastStopDepth) {
             nextDepth = SURFACE_DEPTH;
